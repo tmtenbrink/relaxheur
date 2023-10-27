4e29fb9dfa67384ef5785fd54733d04a29e21538
jaspermulder99 <jaspermulder99@hotmail.nl>
2023-10-27 17:54:51 +0200

    heel veel gefuck

--- test_mincut.py
+++ test_mincut.py
@@ -3,6 +3,7 @@ import itertools
 import numpy as np
 import networkx as nx
 import random
+from itertools import islice
 
 
 def edge_idx(lower_i, higher_j, n: int):
@@ -55,14 +56,16 @@ def check_subtour(x_values: np.ndarray, n: int):
 
 def adjacency_matrix_from_vector(x_values: np.ndarray, n: int):
     """Create an adjacency matrix from x_values"""
-    adjacency_matrix = np.array([[0] * n for _ in range(n)])
+    if len(x_values) != (n * (n - 1)) // 2:
+        raise ValueError("x_values does not have the correct length")
+
+    adjacency_matrix = np.zeros((n, n))
 
     for i in range(n):
         for j in range(i + 1, n):
             idx = edge_idx(i, j, n)
-            if x_values[idx] == 1:
-                adjacency_matrix[i][j] = 1
-                adjacency_matrix[j][i] = 1
+            adjacency_matrix[i][j] = x_values[idx]
+            adjacency_matrix[j][i] = adjacency_matrix[i][j]
 
     return adjacency_matrix
 
@@ -119,10 +122,13 @@ def partition_to_edge_idx(n: int, partition: tuple[list, list]):
     return edge_idx(edges[:, 0], edges[:, 1], n)
 
 
-def compute_min_cut(x_values: np.ndarray, n: int, base_graph: nx.Graph, edge_tuples_arr: np.ndarray):
+def compute_min_cut(
+    x_values: np.ndarray, n: int, base_graph: nx.Graph, edge_tuples_arr: np.ndarray
+):
     """Compute min cut using Stoer–Wagner algorithm using Networkx. x_values should be 1D array with our edge
     index convention. edge_tuples_arr should be of the same length, but here each element is a 3-element array
-    (so an m*3 array) that is (i, j, w) with i the lower vertex index, j the higher and w to be used for weight."""
+    (so an m*3 array) that is (i, j, w) with i the lower vertex index, j the higher and w to be used for weight.
+    """
     # We make a copy so we do not modify the base graph
     weighted_graph = base_graph.copy()
     # We copy the array with weights zero
@@ -142,7 +148,9 @@ def get_cut_idx_arr(vertices: np.ndarray, n: int):
     # each column is the vertex repeated
     vertices_repeated = np.tile(vertices, (n - 1, 1))
     # each column is the index from 0 to n-1
-    indexes_repeated = np.repeat(np.arange(n - 1), vertices_len).reshape((n - 1, vertices_len))
+    indexes_repeated = np.repeat(np.arange(n - 1), vertices_len).reshape(
+        (n - 1, vertices_len)
+    )
     # boolean mask of elements where index is less than vertex
     lt_mask = indexes_repeated < vertices_repeated
     # boolean mask of where index is greater/equal than vertex
@@ -152,8 +160,12 @@ def get_cut_idx_arr(vertices: np.ndarray, n: int):
     indexes_repeated[gt_mask] += 1
 
     # we can now use edge_idx as before
-    vertices_repeated[gt_mask] = edge_idx(vertices_repeated[gt_mask], indexes_repeated[gt_mask], n)
-    vertices_repeated[lt_mask] = edge_idx(indexes_repeated[lt_mask], vertices_repeated[lt_mask], n)
+    vertices_repeated[gt_mask] = edge_idx(
+        vertices_repeated[gt_mask], indexes_repeated[gt_mask], n
+    )
+    vertices_repeated[lt_mask] = edge_idx(
+        indexes_repeated[lt_mask], vertices_repeated[lt_mask], n
+    )
 
     return vertices_repeated
 
@@ -161,28 +173,138 @@ def get_cut_idx_arr(vertices: np.ndarray, n: int):
 def cut_out(vertices: np.ndarray, n: int):
     all_edges = get_cut_idx_arr(vertices, n)
 
-    return all_edges*2
+    return all_edges * 2
 
 
 def cut_in(vertices: np.ndarray, n: int):
     all_edges = get_cut_idx_arr(vertices, n)
 
-    return all_edges * 2+1
+    return all_edges * 2 + 1
 
 
-# n = 6
+def sw_minimum_cut_phase(graph, a):
+    n = len(graph)
+    A = [a]
+    w_list = []
 
-#
-# # adjacency_matrix = adjacency_matrix_from_vector(x_values, n)
-# # compute_min_cut(adjacency_matrix, n)
-#
-# print(check_subtour(x_values, n))
+    while len(A) < n:
+        max_cut_weight = -1
 
-# n = 6
-# x_values = np.array([1, 1.999, 0, 0, 0, 1, 0.33, 0, 0, 1.5, 0, 0, 1, 1, 1])
-# base_graph = nx.complete_graph(n)
-# edge_tuples_arr = get_edge_tpls_arr(n)
-# compute_min_cut(x_values, n, base_graph, edge_tuples_arr)
-n = 6
-vert = np.arange(n)
-print(get_cut_idx_arr(vert, n))
+        for v in range(n):
+            if v not in A:
+                cut_weight = sum(graph[v][w][0] for w in A)
+                if cut_weight > max_cut_weight:
+                    max_cut_weight = cut_weight
+                    u = v
+                    w_list = []
+                    for w in A:
+                        w_list += graph[v][w][1]
+
+        A.append(u)
+
+    s = min(A[-1], A[-2])
+    t = max(A[-1], A[-2])
+
+    return s, t, cut_weight, w_list
+
+
+def sw_minimum_cut(graph):
+    n = len(graph)
+
+    min_cut = float("inf")
+    contractions = []
+    phase = 0
+    best_w_list = []
+
+    while n > 1:
+        a = 0  # Any vertex from V
+        s, t, cut_weight, w_list = sw_minimum_cut_phase(graph[:n][:n], a)
+        if cut_weight < min_cut:
+            min_cut = cut_weight
+            best_phase = phase
+            best_w_list = w_list
+
+        # Merge vertices s and t
+        contractions.append((s, t))
+        print(s, t, cut_weight)
+        for i in range(n):
+            if i != t:
+                graph[s][i] = [
+                    graph[s][i][0] + graph[t][i][0],
+                    graph[s][i][1] + graph[t][i][1],
+                ]
+                graph[i][s] = graph[s][i]
+
+        for i in range(t, n - 1):
+            for j in range(n):
+                graph[i][j] = graph[i + 1][j]
+                graph[j][i] = graph[i][j]
+
+        n -= 1
+        phase += 1
+    # Recover partition
+    print(contractions)
+    for i in islice(contractions, best_phase):
+        print(i)
+
+    v = contractions[best_phase][1]
+
+    return min_cut, phase, best_w_list
+
+
+graph = [
+    [
+        [0, [(0, 0)]],
+        [5, [(0, 1)]],
+        [0, [(0, 2)]],
+        [0, [(0, 3)]],
+        [1, [(0, 4)]],
+        [4, [(0, 5)]],
+    ],
+    [
+        [5, [(1, 0)]],
+        [0, [(1, 1)]],
+        [2, [(1, 2)]],
+        [0, [(1, 3)]],
+        [0, [(1, 4)]],
+        [0, [(1, 5)]],
+    ],
+    [
+        [0, [(2, 0)]],
+        [2, [(2, 1)]],
+        [0, [(2, 2)]],
+        [6, [(2, 3)]],
+        [1, [(2, 4)]],
+        [1, [(2, 5)]],
+    ],
+    [
+        [0, [(3, 0)]],
+        [0, [(3, 1)]],
+        [6, [(3, 2)]],
+        [0, [(3, 3)]],
+        [3, [(3, 4)]],
+        [0, [(3, 5)]],
+    ],
+    [
+        [1, [(4, 0)]],
+        [0, [(4, 1)]],
+        [1, [(4, 2)]],
+        [3, [(4, 3)]],
+        [0, [(4, 4)]],
+        [0, [(4, 5)]],
+    ],
+    [
+        [4, [(5, 0)]],
+        [0, [(5, 1)]],
+        [1, [(5, 2)]],
+        [0, [(5, 3)]],
+        [0, [(5, 4)]],
+        [0, [(5, 5)]],
+    ],
+]
+# G = nx.from_numpy_array(graph)
+# print(nx.is_connected(G))
+# cut_value, partition = nx.stoer_wagner(G)
+# print(cut_value, partition)
+
+print(sw_minimum_cut(graph))
